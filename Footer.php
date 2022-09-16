07ce8c75bc82683060afac763bce522b58e9835c
Anita Mjeku <113339643+anitamje@users.noreply.github.com>
2022-09-16 16:52:39 +0200

    Create Footer.php

---
+++ Footer.php
@@ -0,0 +1,48 @@
+<div class="footer">
+        <div class="fitem-1">
+            <h2>Navigation</h2>
+            <div class="footer-nav">
+                <ul>
+                    <li><a href="index.php">Home</a></li>
+                    <li><a href="categories.php">Categories</a></li>
+                    <li><a href="about.php">About</a></li>
+                    <li><a href="contact.php">Contact</a></li>
+                </ul>
+            </div>
+        </div>
+        <div class="fitem-2">
+                <h2>Newsletter</h2>
+                <div class"newsletter">
+                    <p>Enter your email address and subcribe daily newsletter</p>
+                    <form method="post">
+                        <input class="email-form"type="email" placeholder="johdoe@example.com">
+                        <button class="submit-button"type="submit" data-validation="required email">Subscribe</button>
+                    </form>
+                </div>
+    
+                <div class="social">
+                    <a href=""><i class="fab fcolor fa-facebook fa-2x"></i></a>
+                    <a href=""><i class="fab tcolor fa-twitter fa-2x"></i></a>
+                    <a href=""><i class="fab licolor fa-linkedin-in fa-2x"></i></a>
+                    <a href=""><i class="fab instcolor fa-instagram fa-2x"></i></a>
+                    <a href=""><i class="fab mediumcolor fa-medium fa-2x"></i></a>
+                    
+                </div>
+    
+            </div>
+        <div class="fitem-3">
+            <h2>Our app available</h2>
+            <div class="our-app">
+                <div class="ios-app">
+                    <a href="#"><img src="images/apple.svg" width="50px"></a>
+                    <p style="text-align: center">IOS</p>
+                </div>
+                <div class="android-app">
+                    <a style="padding-left: 10px" href="#"><img src="images/android.svg" width="50px" height="60px"></a>
+                    <p style="text-align: center">ANDROID</p>
+                </div>
+                
+            </div>
+            
+        </div>
+        </div>
