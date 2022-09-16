e8db4a4ee2dc7e25493b2c2676dd27d2be22d5f2
Anita Mjeku <113339643+anitamje@users.noreply.github.com>
2022-09-16 16:59:21 +0200

    Create Index.php

---
+++ Index.php
@@ -0,0 +1,53 @@
+<?php 
+include('includes/header.php');
+
+?>
+
+  
+    <div id="slider">
+        <figure>
+            <img src ="images/simages1.png">
+            <img src ="images/simages2.jpg">
+            <img src ="images/simages3.jpg">
+        </figure>
+    </div>
+
+        
+        
+    </div>
+
+    <div class="home-categories home-about">
+        
+        <div class="layout-cont1">
+        
+        <div class="layout-item1">
+            <img style="height:250px;width:100%" src="images/about2.png">
+           
+        </div>
+        
+        <div class="layout-item1">
+            <h3>About</h3>
+            <p>Laboris quem duis admodum fore, te quamquam distinguantur. Quo de dolor illum 
+                amet. Incurreret aute eiusmod ullamco an quis consequat se ullamco, litteris 
+                philosophari et commodo te fore ingeniis praetermissum e laborum quae magna 
+                voluptate quem, sed nisi eruditionem est et expetendis eu occaecat ubi aute iis 
+                officia. Senserit quid si vidisse adipisicing, irure e admodum ab labore. Aliqua 
+                nam et elit aliquip quo dolore appellat in excepteur, te iudicem ab litteris, 
+                proident ita legam quibusdam. Lorem cupidatat cohaerescant. Ita nulla occaecat 
+                fidelissimae ubi ubi anim reprehenderit quo o de sempiternum in hic magna hic 
+                elit.</p>
+            <a class="button2" href="about.php">About</a>
+        </div>
+        </div>     <!--layout-cont1 ends here-->   
+    </div>
+   
+
+    
+
+    </body>
+
+
+    <script src="./js/jquery-3.6.0.min.js"></script>
+
+    
+</html>
